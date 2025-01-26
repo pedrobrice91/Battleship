0d0903db617c63936973221d7946ff24976e2fbb
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2025-01-26 19:11:55 -0300

    seconds

---
+++ src/App.js
@@ -0,0 +1,293 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Battleship!</h1>
+      <div className="">
+      <div className="">
+      <div className="d-flex">
+        <div id="filaUno"></div>
+        <div id="filaUno">1</div>
+        <div id="filaUno">2</div>
+        <div id="filaUno">3</div>
+        <div id="filaUno">4</div>
+        <div id="filaUno">5</div>
+        <div id="filaUno">6</div>
+        <div id="filaUno">7</div>
+        <div id="filaUno">8</div>
+        <div id="filaUno">9</div>
+        <div id="filaUno">10</div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >A</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >B</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >C</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >D</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >E</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >F</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >G</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >H</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      <div className="d-flex">
+        <div id="filaUno" >I</div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+        <div id="filaUno" className="border border-primary" ></div>
+      </div>
+      
+    </div>
+    <div className="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+    
+      </div>
+      <div className="">
+        
+        <div className="d-flex">
+          <div id="filaUno" >A</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >B</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >C</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >D</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >E</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >F</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >G</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >H</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        <div className="d-flex">
+          <div id="filaUno" >I</div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+          <div id="filaUno" className="border border-primary" ></div>
+        </div>
+        
+      </div>
+      </div>
+      <button type="button" className="btn btn-success">Iniciar Juego</button>
+      <div class="col-md-4">
+    <label for="inputState" class="form-label">Selecciona el barco</label>
+    <select id="inputState" class="form-select">
+      <option selected>Choose...</option>
+      <option>PortaAvion(5 casillas)</option>
+      <option>Acorazado(4 casillas)</option>
+      <option>Destructor (3 casillas)</option>
+      <option>Submarino(3 casillas)</option>
+      <option>crucero(2 casillas)</option>
+    </select>
+  </div>
+      <div class="col-md-6">
+    <label for="inputCity" class="form-label">columna</label>
+    <input type="text" class="form-control" id="inputCity"/>
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">fila</label>
+    <input type="text" class="form-control" id="inputCity"/>
+  </div>
+  <button type="button" className="btn btn-success">Atacar</button>
+
+    </div>
+    
+  );
+}
+
+export default App;
